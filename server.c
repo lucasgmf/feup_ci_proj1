fe56f1622555c32565b876a15dc06666630f9bf5
lucasgmf <lucasgalvaomfreitas@gmail.com>
2023-09-28 09:41:34 +0100

    added initial files

---
+++ server.c
@@ -0,0 +1,10 @@
+#include "server.h"
+
+#include <stdio.h>
+
+
+int main() {
+    int so, sd, len;
+    struct sockaddr_in serverAddr, clientAddr; // local and remote addresses
+    // socklen_t addr_size = sizeof()
+}
